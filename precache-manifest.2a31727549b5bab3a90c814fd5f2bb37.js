c78f2fb4986d7d1a70700e5bf5ca4115d2c1a53b
ibrahimtaj <taj_it@yahoo.com>
2020-05-10 15:48:43 +0530

    Updates

---
+++ precache-manifest.2a31727549b5bab3a90c814fd5f2bb37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9937a618d864d8668c397bfbe23d9d38",
+    "url": "/my-salad/index.html"
+  },
+  {
+    "revision": "1dee98cd7791db1b7d02",
+    "url": "/my-salad/static/css/main.b63f7fa6.chunk.css"
+  },
+  {
+    "revision": "fe095ae966e9e789412d",
+    "url": "/my-salad/static/js/2.9b6f253e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-salad/static/js/2.9b6f253e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dee98cd7791db1b7d02",
+    "url": "/my-salad/static/js/main.0a36c14f.chunk.js"
+  },
+  {
+    "revision": "3a827c3d376cb180a7df",
+    "url": "/my-salad/static/js/runtime-main.db9b347b.js"
+  }
+]);
